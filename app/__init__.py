0f644d4bf2368f1318b310af05bf0cbb48eb4955
KadoProG <splatrain@gmail.com>
2024-06-26 13:06:39 +0900

    update scssに対応

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_assets import Environment, Bundle
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,8 +13,14 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Flask-Assetsの設定
+    assets = Environment(app)
+    scss = Bundle('src/scss/styles.scss', filters='libsass', output='dist/css/styles.css')
+    assets.register('scss_all', scss)
+    scss.build()
+
     with app.app_context():
-        from app import routes
+        from app import routes, models
         db.create_all()
 
     return app
